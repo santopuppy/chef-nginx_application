76d2f9fb3bef03a667fb489e65c7fbf6827840d5
Lester Celestial <lesterc@sourcepad.com>
2015-06-09 01:03:42 +0800

    updated the metadata

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name 'nginx_ruby_backend'
+name 'nginx_application'
 maintainer 'Lester Celestial'
 maintainer_email 'lesterc@sourcepad.com'
 license 'Apache 2.0'
@@ -6,7 +6,7 @@ description 'Installs nginx and configures virtual hosts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-recipe 'nginx_ruby_backend', "Installs and configures nginx on a server."
+recipe 'nginx_application', "Installs and configures nginx on a server."
 
 depends 'apt'
 depends 'nginx'
